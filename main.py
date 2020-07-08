6f1208429db86a091f318b6c398af457d2c469a5
codeo <alphaCodeo@users.noreply.github.com>
2020-07-08 14:09:42 +0800

    No errors. Backprop not working, array maths probably inefficient.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # TODO: object-oriented?
 import numpy as np
+import numba
 import mlp
 
-epochs = 1
-learning_rate = 0.1
+epochs = 10000
+learning_rate = 0.5
 
 node_count = (2, 2, 1)
 layer_count = len(node_count)
@@ -11,14 +12,33 @@ layer_count = len(node_count)
 inputs = np.array([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
 expected = np.array([[0.0], [1.0], [1.0], [0.0]])
 
-weights = [np.random.uniform(low=-1.0, high=1.0, size=(i, 3)) \
+weights = numba.typed.List()
+[weights.append(np.random.uniform(low=-1.0, high=1.0, size=(i, 3))) \
     for i in node_count]
 
-[print(mlp.forward_prop(i, weights)) for i in inputs]
+print(weights)
+print()
+for i in inputs:
+    r = mlp.forward_prop(i, weights)
+    for j in r:
+        print(j)
+    print()
 
 for _ in range(epochs):
-    for curr_input in inputs:
-        activations = mlp.forward_prop(inputs[curr_input], weights)
+    for curr_input in range(inputs.shape[0]):
+        activations = numba.typed.List()
+        a = mlp.forward_prop(inputs[curr_input], weights)
+        [activations.append(i) for i in a]
 
-        weights = mlp.backprop(inputs[curr_input], \
-            expected[curr_input], weights)
+        w = mlp.backprop(inputs[curr_input], expected[curr_input], \
+            activations, weights)
+        weights = numba.typed.List()
+        [weights.append(i) for i in w]
+
+print(weights)
+print()
+for i in inputs:
+    r = mlp.forward_prop(i, weights)
+    for j in r:
+        print(j)
+    print()
--- mlp.py
+++ mlp.py
@@ -45,9 +45,34 @@ def forward_prop(inputs, weights):
     return activations
 
 @numba.jit(nopython=True)
-def backprop(inputs, expected, weights):
+def backprop(inputs, expected, activations, weights, learning_rate=0.1):
     """Runs one iteration of forward propagation through the neural net, then
     backpropagates and returns updated weights."""
-    for i in reversed(range(len(weights))):
+    weight_delta = [np.empty(i.shape) for i in weights]
+    dstream_error_term = np.empty(1)
+
+    for _ in range(len(weights)):
+        i = len(weights) - _ - 1
+        if i == 0:
+            break
+
+        curr_error_term = np.empty(weights[i].shape[0])
+
         for j in range(weights[i].shape[0]):
-            pass
+            ustream_activation = np.append(activations[i-1], 1)
+
+            if i == len(weights) - 1:
+                activation_error = activations[-1][j] - expected[j]
+            else:
+                activation_error = np.sum(np.multiply(dstream_error_term, \
+                    weights[i]))
+
+            error_term = activation_prime(activations[i][j]) * activation_error
+
+            weight_delta[i][j] = learning_rate * \
+                error_term * ustream_activation
+
+            curr_error_term[j] = error_term
+        dstream_error_term = curr_error_term
+
+    return [weights[i] - weight_delta[i] for i in range(len(weights))]
